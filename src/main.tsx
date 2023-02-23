25f31378f29543e64b882f7db7dc57f379076d01
azamatcodes <azamatcodes@gmail.com>
2023-02-24 01:21:02 +0500

    Add Redux Toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,19 @@ import './styles/scss/index.scss'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import theme from './theme'
 import { Fonts } from './components/Fonts'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <Fonts />
-      <App />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Fonts />
+        <App />
+      </ChakraProvider>
+    </Provider>
   </StrictMode>
 )
