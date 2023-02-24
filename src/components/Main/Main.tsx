bb811b847cc878b0330d9b2cb83a3f45f7f862b8
azamatcodes <azamatcodes@gmail.com>
2023-02-24 14:23:04 +0500

    Testing

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,7 +3,7 @@ import { Box, Container } from '@chakra-ui/react'
 export const Main = () => {
   return (
     <Box className="Main" w="100%">
-      <Container maxW="container.xl">MAIN</Container>
+      <Container maxW="container.xl">MAIN SECTION</Container>
     </Box>
   )
 }
