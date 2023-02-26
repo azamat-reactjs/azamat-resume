7f61f449fd12e2429649af253e41fa5abad83360
azamatcodes <azamatcodes@gmail.com>
2023-02-27 01:49:00 +0500

    Add contact section html part (demo)

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -21,18 +21,27 @@ export const Contact = () => {
           <div className="contact__info">
             <div className="contact__data">
               <span className="contact__data-title">Email</span>
-              <span className="contact__data-infp">azamatreactjs@gmail.com</span>
+              <span className="contact__data-infp">
+                azamatreactjs@gmail.com
+              </span>
 
-              <a className="contact__button" href="mailto:azamatreactjs@gmail.com">
+              <a
+                className="contact__button"
+                href="mailto:azamatreactjs@gmail.com"
+              >
                 Write me <ArrowRightLine />
               </a>
             </div>
-            
+
             <div className="contact__data">
               <span className="contact__data-title">Telegram</span>
               <span className="contact__data-infp">+998 (95) 004-40-48</span>
 
-              <a className="contact__button" href="https://t.me/azamatreactjs" target="_blank">
+              <a
+                className="contact__button"
+                href="https://t.me/azamatreactjs"
+                target="_blank"
+              >
                 Write me <ArrowRightLine />
               </a>
             </div>
@@ -41,7 +50,11 @@ export const Contact = () => {
               <span className="contact__data-title">Instagram</span>
               <span className="contact__data-infp">@azamat_reactjs</span>
 
-              <a className="contact__button" href="https://instagram.com/azamat_reactjs" target="_blank">
+              <a
+                className="contact__button"
+                href="https://instagram.com/azamat_reactjs"
+                target="_blank"
+              >
                 Write me <ArrowRightLine />
               </a>
             </div>
@@ -55,16 +68,39 @@ export const Contact = () => {
 
           <form className="contact__form" id="contact-form">
             <div className="contact-form-div">
-              <label className="contact__form-tag" htmlFor="full-name">Full Name</label>
-              <input className="contact__form-input" id="full-name" placeholder="Write your full name" type="text" name="full-name" />
+              <label className="contact__form-tag" htmlFor="full-name">
+                Full Name
+              </label>
+              <input
+                className="contact__form-input"
+                id="full-name"
+                placeholder="Write your full name"
+                type="text"
+                name="full-name"
+              />
             </div>
             <div className="contact-form-div">
-              <label className="contact__form-tag" htmlFor="email">Email</label>
-              <input className="contact__form-input" id="email" placeholder="Write your email" type="email" name="email" />
+              <label className="contact__form-tag" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="contact__form-input"
+                id="email"
+                placeholder="Write your email"
+                type="email"
+                name="email"
+              />
             </div>
             <div className="contact-form-div contact__form-area">
-              <label className="contact__form-tag" htmlFor="project">Project</label>
-              <textarea className="contact__form-input" id="project" placeholder="Write your project" name="project"></textarea>
+              <label className="contact__form-tag" htmlFor="project">
+                Project
+              </label>
+              <textarea
+                className="contact__form-input"
+                id="project"
+                placeholder="Write your project"
+                name="project"
+              ></textarea>
             </div>
 
             <button className="contact__button" type="submit">
